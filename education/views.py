ecc716b8fa3c9090387343dba7f8f828f78b1199
Pom(Paul) Kim <tigers2020@gmail.com>
2024-07-15 14:41:08 -0400

    update color scheme

--- education/views.py
+++ education/views.py
@@ -1,56 +1,76 @@
-# src/education/views.py
-
-# Import necessary modules
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-
 from .models import Scenario, Page, Choice, Outcome
 
-
-# Class-based view to display multiple scenarios
 class ScenarioListView(ListView):
-    model = Scenario  # Model to use
-    template_name = 'components/pages/education/scenario_list.html'  # Template file to use
-    context_object_name = 'scenarios'  # Name for the list as a template variable
+    model = Scenario
+    template_name = 'components/pages/education/scenario_list.html'
+    context_object_name = 'scenarios'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['breadcrumbs'] = [
-            {'name': 'Home', 'url': '/'},
-            {'name': 'Scenarios', 'url': None}]  # current page doesn't have a URL
+            {'name': 'Home', 'url': reverse_lazy('home')},
+            {'name': 'Scenarios', 'url': None}  # Current page doesn't have a URL
+        ]
         return context
 
-
-# Class-based view to display single scenario details
 class ScenarioDetailView(DetailView):
-    model = Scenario  # Model to use
-    template_name = 'components/pages/education/scenario_detail.html'  # Template file to use
-    context_object_name = 'scenario'  # Name to use for this model as template variable
+    model = Scenario
+    template_name = 'components/pages/education/scenario_detail.html'
+    context_object_name = 'scenario'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['breadcrumbs'] = [
-            {'name': 'Home', 'url': '/'},
-            {'name': 'Scenarios', 'url': '/scenarios/'},
-            {'name': self.object.name, 'url': None}]  # current page doesn't have a URL
+            {'name': 'Home', 'url': reverse_lazy('home')},
+            {'name': 'Scenarios', 'url': reverse_lazy('scenario_list')},
+            {'name': self.object.title, 'url': None}  # Current page doesn't have a URL
+        ]
         return context
 
-
-# Class-based view to display single page details
 class PageDetailView(DetailView):
-    model = Page  # Model to use
-    template_name = 'components/pages/education/page_detail.html'  # Template file to use
-    context_object_name = 'page'  # Name to use for this model as template variable
+    model = Page
+    template_name = 'components/pages/education/page_detail.html'
+    context_object_name = 'page'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        page = self.get_object()
+        context['breadcrumbs'] = [
+            {'name': 'Home', 'url': reverse_lazy('home')},
+            {'name': 'Scenarios', 'url': reverse_lazy('scenario_list')},
+            {'name': page.scenario.title, 'url': reverse_lazy('scenario_detail', kwargs={'pk': page.scenario.pk})},
+            {'name': f'Page {page.order}', 'url': None}  # Current page doesn't have a URL
+        ]
+        return context
 
-# Class-based view to display single choice details
 class ChoiceDetailView(DetailView):
-    model = Choice  # Model to use
-    template_name = 'components/pages/education/choice_detail.html'  # Template file to use
-    context_object_name = 'choice'  # Name to use for this model as template variable
+    model = Choice
+    template_name = 'components/pages/education/choice_detail.html'
+    context_object_name = 'choice'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['breadcrumbs'] = [
+            {'name': 'Home', 'url': reverse_lazy('home')},
+            {'name': 'Scenarios', 'url': reverse_lazy('scenario_list')},
+            {'name': 'Choices', 'url': None},  # Parent breadcrumb
+            {'name': self.object.text, 'url': None}  # Current page doesn't have a URL
+        ]
+        return context
 
-# Class-based view to display single outcome details
 class OutcomeDetailView(DetailView):
-    model = Outcome  # Model to use
-    template_name = 'components/pages/education/outcome_detail.html'  # Template file to use
-    context_object_name = 'outcome'  # Name to use for this model as template variable
+    model = Outcome
+    template_name = 'components/pages/education/outcome_detail.html'
+    context_object_name = 'outcome'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['breadcrumbs'] = [
+            {'name': 'Home', 'url': reverse_lazy('home')},
+            {'name': 'Scenarios', 'url': reverse_lazy('scenario_list')},
+            {'name': 'Outcomes', 'url': None},  # Parent breadcrumb
+            {'name': f'Outcome for {self.object.page}', 'url': None}  # Current page doesn't have a URL
+        ]
+        return context
--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('./tailwind.colors');
+
 module.exports = {
     content: [
         '../templates/**/*.html',
@@ -10,79 +12,18 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                black: '#000000',
-                white: '#FFFFFF',
-                neutral: {
-                    50: '#FAFAFA',
-                    100: '#F5F5F5',
-                    200: '#EEEEEE',
-                    300: '#E1E1E1',
-                    400: '#CACACA',
-                    500: '#8E8E8E',
-                    600: '#4B4B4B',
-                    700: '#1F1F1F',
-                },
-                primary: {
-                    50: '#FFFDF2',
-                    100: '#FCFAEB',
-                    200: '#F7F5E8',
-                    300: '#F4F4EA',
-                    400: '#F4F1E6',
-                    500: '#F2EFE3',
-                    600: '#F2EFE4',
-                    700: '#F2E0E6',
-                },
-                secondary: {
-                    50: '#FFF6EE',
-                    100: '#FFF3CB',
-                    200: '#FFD6B2',
-                    300: '#FFC99A',
-                    400: '#FFB677',
-                    500: '#FFAC64',
-                    600: '#FF9407',
-                    700: '#F07000',
-                },
-                danger: {
-                    50: '#FFFBFB',
-                    100: '#FFD7E0',
-                    200: '#FFEBEE',
-                    300: '#FFCCD2',
-                    400: '#F49898',
-                    500: '#EB6F70',
-                    600: '#F64C4C',
-                    700: '#EC2D30',
-                },
-                warning: {
-                    50: '#FFFDFA',
-                    100: '#FFF9EE',
-                    200: '#FFF7E1',
-                    300: '#FFEAB3',
-                    400: '#FFDD82',
-                    500: '#FFC62B',
-                    600: '#FFAD0D',
-                    700: '#FE9B0E',
-                },
-                success: {
-                    50: '#FBFEFC',
-                    100: '#F2FAF6',
-                    200: '#E5F5EC',
-                    300: '#00D4B4',
-                    400: '#97D4B4',
-                    500: '#6BC497',
-                    600: '#47B881',
-                    700: '#0C9D61',
-                },
-                info: {
-                    50: '#F8FCFF',
-                    100: '#F1F8FF',
-                    200: '#E4F2FF',
-                    300: '#BDDDFF',
-                    400: '#93C8FF',
-                    500: '#4BA1FF',
-                    600: '#3B82F6',
-                    700: '#3A70E2',
-                }
-            }
+                black: colors.black,
+                white: colors.white,
+                neutral: colors.neutral.light,
+                primary: colors.primary.light,
+                secondary: colors.secondary,
+                danger: colors.danger,
+                warning: colors.warning,
+                success: colors.success,
+                info: colors.info,
+                'dark-neutral': colors.neutral.dark,
+                'dark-primary': colors.primary.dark,
+            },
         }
     },
     variants: {
@@ -97,4 +38,4 @@ module.exports = {
         require('@tailwindcss/aspect-ratio'),
         require('autoprefixer'),
     ],
-}
+};
