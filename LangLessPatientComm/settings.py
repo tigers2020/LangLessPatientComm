616dab846f2b454fcb8fa55d3a74c49ff7c40932
tigers2020 <tigers2020@hotmail.com>
2024-07-15 15:13:42 -0400

    allowed_hosts setting update

--- LangLessPatientComm/settings.py
+++ LangLessPatientComm/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['129.21.101.86', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
